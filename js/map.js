38d9ae0ccaace4f45273cb560d478828d16cac83
zuosang51-art <zuosang51@gmail.com>
2026-06-20 23:43:03 +0800

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,18 +1,16 @@
 
 let map;
 
-// 🟢 KML 永久图层（最高层）
+// 🟢 KML 永久顶层
 let kmlLayerGroup = L.layerGroup();
 
 function initMap() {
 
   map = L.map("map").setView([48.5, 37.7], 6);
 
-  L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
-    maxZoom: 18
-  }).addTo(map);
+  L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png").addTo(map);
 
-  // 🟢 KML层（始终最高）
+  // 🟢 KML层（永远在最上层）
   kmlLayerGroup.addTo(map);
   kmlLayerGroup.setZIndex(99999);
 }
