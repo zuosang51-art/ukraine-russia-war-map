26659c3d44cc12559cfd6b6aacbd730c9f9e7f31
zuosang51-art <zuosang51@gmail.com>
2026-06-20 22:32:33 +0800

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -1,17 +1,38 @@
 let drawnItems = new L.FeatureGroup();
+let currentLayer = null;
+
+// 默认样式
+const defaultStyle = {
+  color: "#ff3b30",
+  weight: 3,
+  dashArray: "0"
+};
+
+function applyStyle(layer, style) {
+  layer.setStyle({
+    color: style.color,
+    weight: style.weight,
+    dashArray: style.dashArray,
+    opacity: 1
+  });
+
+  layer._styleConfig = style;
+}
 
 function initDraw() {
 
   map.addLayer(drawnItems);
 
   const drawControl = new L.Control.Draw({
-    edit: { featureGroup: drawnItems },
+    edit: {
+      featureGroup: drawnItems
+    },
     draw: {
       polygon: true,
       polyline: true,
       rectangle: true,
-      circle: false,
       marker: true,
+      circle: false,
       circlemarker: false
     }
   });
@@ -21,6 +42,9 @@ function initDraw() {
   map.on(L.Draw.Event.CREATED, function (e) {
 
     const layer = e.layer;
+
+    applyStyle(layer, defaultStyle);
+
     drawnItems.addLayer(layer);
 
     // 面积计算
@@ -29,13 +53,10 @@ function initDraw() {
       layer.bindPopup("Area: " + (area / 1000000).toFixed(2) + " km²");
     }
 
-    // 标签编辑
-    layer.bindPopup(`
-      <div class="popup">
-        <b>战线标记</b><br>
-        可插入图片 / 视频<br>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg" width="150"/>
-      </div>
-    `);
+    layer.bindPopup("Click to edit style");
+
+    layer.on("click", function () {
+      openStylePanel(layer);
+    });
   });
 }
