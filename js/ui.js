68722e95dd3a07148efe3b1c8531837c8b947abd
zuosang51-art <zuosang51@gmail.com>
2026-06-21 00:28:36 +0800

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,23 @@
-document.addEventListener("GIS_READY", initUI);
+document.addEventListener("GIS_READY",()=>{
+
+  window.enableLabelMode = function(){
+
+    const text = prompt("Label:");
+    if(!text) return;
+
+    GIS.map.on("click", function handler(e){
+
+      L.marker(e.latlng,{
+        icon:L.divIcon({
+          className:"",
+          html:"<div style='color:#0ff;background:#000a;padding:2px;border:1px solid #0ff'>"+text+"</div>"
+        })
+      }).addTo(GIS.map);
+
+      GIS.map.off("click", handler);
+
+    });
+
+  };
+
+});
