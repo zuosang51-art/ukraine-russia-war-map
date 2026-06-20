55d1729db7d546a07d88e1aaaec98c95b666a6d7
zuosang51-art <zuosang51@gmail.com>
2026-06-20 23:12:19 +0800

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -1,48 +1,11 @@
 let drawnItems = new L.FeatureGroup();
 
-function applyStyle(layer, style) {
-
-  if (layer.setStyle) {
-    layer.setStyle(style);
-  }
-
-  layer._styleConfig = style;
-}
-
 function initDraw() {
-
   map.addLayer(drawnItems);
 
-  const drawControl = new L.Control.Draw({
-    edit: { featureGroup: drawnItems },
-    draw: {
-      polygon: true,
-      polyline: false,
-      rectangle: true,
-      marker: true,
-      circle: false
-    }
-  });
+  // 🟢 开启箭头绘制模式
+  enableArrowDrawing();
 
-  map.addControl(drawControl);
-
-  map.on(L.Draw.Event.CREATED, function (e) {
-
-    const layer = e.layer;
-
-    applyStyle(layer, {
-      color: getColorByIndex(drawnItems.getLayers().length),
-      weight: 3,
-      opacity: 1,
-      dashArray: "0"
-    });
-
-    drawnItems.addLayer(layer);
-
-    layer.on("click", () => openStylePanel(layer));
-  });
-
-  // 🆕 战线箭头API（接入UI模式）
   window.createBattleArrow = function(from, to, type) {
 
     const color = getColorByIndex(drawnItems.getLayers().length);
@@ -52,7 +15,7 @@ function initDraw() {
       weight: 3,
       opacity: 1,
       dashArray: "0",
-      type: type || currentArrowMode
+      type: type
     });
 
     drawnItems.addLayer(arrow);
