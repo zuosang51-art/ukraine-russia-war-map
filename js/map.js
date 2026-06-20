30ec0cff60c1214b1596c169d53ee99b1cd3571c
zuosang51-art <zuosang51@gmail.com>
2026-06-20 23:38:05 +0800

    Refactor map initialization and add KML layer
    Refactor map initialization and layer control. Removed old layer control and added KML layer group with higher z-index.

--- js/map.js
+++ js/map.js
@@ -1,48 +1,18 @@
+
 let map;
-let layers = {};
+
+// 🟢 KML 永久图层（最高层）
+let kmlLayerGroup = L.layerGroup();
 
 function initMap() {
 
-  layers.topo = L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png");
-  layers.sat = L.tileLayer("https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}");
-  layers.street = L.tileLayer("https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}");
+  map = L.map("map").setView([48.5, 37.7], 6);
 
-  map = L.map("map", {
-    center: [48.5, 37.7],
-    zoom: 6,
-    layers: [layers.topo]
-  });
+  L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
+    maxZoom: 18
+  }).addTo(map);
 
-  layers.topo.addTo(map);
-
-  // 🟢 右上角图层控制
-  const LayerControl = L.Control.extend({
-    options: { position: "topright" },
-
-    onAdd: function () {
-      const div = L.DomUtil.create("div", "layer-box");
-
-      div.innerHTML = `
-        <button id="t">Topo</button>
-        <button id="s">Sat</button>
-        <button id="st">Street</button>
-      `;
-
-      L.DomEvent.disableClickPropagation(div);
-      return div;
-    }
-  });
-
-  map.addControl(new LayerControl());
-
-  setTimeout(() => {
-    document.getElementById("t").onclick = () => switchLayer("topo");
-    document.getElementById("s").onclick = () => switchLayer("sat");
-    document.getElementById("st").onclick = () => switchLayer("street");
-  }, 100);
-}
-
-function switchLayer(type) {
-  Object.values(layers).forEach(l => map.removeLayer(l));
-  layers[type].addTo(map);
+  // 🟢 KML层（始终最高）
+  kmlLayerGroup.addTo(map);
+  kmlLayerGroup.setZIndex(99999);
 }
