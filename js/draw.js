cdcc0c4658fad46629e590f5b837eba9ef7f945a
zuosang51-art <zuosang51@gmail.com>
2026-06-21 00:27:14 +0800

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -1,1 +1,16 @@
-document.addEventListener("GIS_READY", initDraw);
+document.addEventListener("GIS_READY",()=>{
+
+  const drawn = new L.FeatureGroup().addTo(GIS.map);
+
+  const control = new L.Control.Draw({
+    edit:{featureGroup:drawn},
+    draw:{polygon:true,rectangle:true,polyline:true,marker:true}
+  });
+
+  GIS.map.addControl(control);
+
+  GIS.map.on(L.Draw.Event.CREATED,(e)=>{
+    drawn.addLayer(e.layer);
+  });
+
+});
