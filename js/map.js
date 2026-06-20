38021aad77aab52143092492a1e9831a98f4c677
zuosang51-art <zuosang51@gmail.com>
2026-06-20 22:23:24 +0800

    Create map.js

---
+++ js/map.js
@@ -0,0 +1,31 @@
+let map;
+
+let layers = {};
+
+function initMap() {
+
+  layers.topo = L.tileLayer(
+    "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
+  );
+
+  layers.sat = L.tileLayer(
+    "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+  );
+
+  layers.street = L.tileLayer(
+    "https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+  );
+
+  map = L.map("map", {
+    center: [48.5, 37.7],
+    zoom: 6,
+    layers: [layers.topo]
+  });
+
+  layers.topo.addTo(map);
+}
+
+function switchLayer(type) {
+  Object.values(layers).forEach(l => map.removeLayer(l));
+  layers[type].addTo(map);
+}
