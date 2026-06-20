62448868c4889a8ab1f791c0e47b46852c6acc8b
zuosang51-art <zuosang51@gmail.com>
2026-06-21 00:26:49 +0800

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,13 +1,7 @@
-document.addEventListener("GIS_READY", () => {
+document.addEventListener("GIS_READY",()=>{
 
-  console.log("map module ready");
-
-  L.control.layers(
-    {
-      "Topographic": GIS.layers.topo,
-      "Satellite": GIS.layers.sat,
-      "Street": GIS.layers.street
-    }
-  ).addTo(GIS.map);
+  L.control.layers({
+    "Base": GIS.layers.base
+  }).addTo(GIS.map);
 
 });
