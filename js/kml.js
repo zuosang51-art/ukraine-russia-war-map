c05714371fa8cc129a0efeac428dde848a2a7188
zuosang51-art <zuosang51@gmail.com>
2026-06-21 00:38:30 +0800

    Update kml.js

--- js/kml.js
+++ js/kml.js
@@ -1,9 +1,28 @@
-document.addEventListener("GIS_READY",()=>{
+function loadKML(){
 
-  window.loadKML = function(){
+  fetch("kml/frontline.kml")
+    .then(r=>r.text())
+    .then(xmlText=>{
 
-    alert("KML loader ready (stub)");
+      const xml = new DOMParser()
+        .parseFromString(xmlText,"text/xml");
 
-  };
+      const coords = xml.getElementsByTagName("coordinates");
 
-});
+      for(let c of coords){
+
+        const pts = c.textContent.trim().split(" ");
+
+        const latlngs = pts.map(p=>{
+          const [lng,lat]=p.split(",");
+          return [parseFloat(lat),parseFloat(lng)];
+        });
+
+        L.polyline(latlngs,{
+          color:"yellow"
+        }).addTo(map);
+      }
+
+    });
+
+}
