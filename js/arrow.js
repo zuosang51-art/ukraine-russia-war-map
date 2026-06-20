ba2cfd9d151c2094676c1e0a262d68d78986b670
zuosang51-art <zuosang51@gmail.com>
2026-06-21 00:38:04 +0800

    Update arrow.js

--- js/arrow.js
+++ js/arrow.js
@@ -1,7 +1,25 @@
-document.addEventListener("GIS_READY",()=>{
+function createArrow(from,to){
 
-  window.setArrowMode = function(mode){
-    window.currentArrowMode = mode;
-  };
+  const dx = to.lng-from.lng;
+  const dy = to.lat-from.lat;
 
-});
+  const len = Math.sqrt(dx*dx+dy*dy);
+
+  const ux = dx/len;
+  const uy = dy/len;
+
+  const px = -uy*0.3;
+  const py = ux*0.3;
+
+  L.polyline([from,to],{color:"red"}).addTo(map);
+
+  L.polygon([
+    [to.lat,to.lng],
+    [to.lat-ux*0.4+px,to.lng-uy*0.4+py],
+    [to.lat-ux*0.8,to.lng-uy*0.8],
+    [to.lat-ux*0.4-px,to.lng-uy*0.4-py]
+  ],{
+    color:"red",
+    fillOpacity:0.7
+  }).addTo(map);
+}
