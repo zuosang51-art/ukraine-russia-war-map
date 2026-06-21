0791b17320ed145b13efc7da022fa401c0f68d1b
zuosang51-art <zuosang51@gmail.com>
2026-06-21 09:23:47 +0800

    Update map.js

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+function initMap(){
 
   const map = L.map("map").setView([48.5,37.7],6);
 
@@ -8,22 +8,19 @@ window.onload = function(){
     "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   ).addTo(map);
 
-  const topo = L.tileLayer(
-    "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
-  );
-
   const sat = L.tileLayer(
     "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
   );
 
+  const topo = L.tileLayer(
+    "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
+  );
+
   L.control.layers({
     "OSM":osm,
-    "Topo":topo,
-    "Satellite":sat
+    "SAT":sat,
+    "TOPO":topo
   }).addTo(map);
 
-  GIS.drawn = new L.FeatureGroup();
-  map.addLayer(GIS.drawn);
-
-  initDraw();
-};
+  bindEvents();
+}
